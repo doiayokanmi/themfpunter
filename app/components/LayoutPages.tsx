521eb332b9ffed1a29ec365e1b1b8c75842d363f
Ibrahim Damilola Dhikirullahi <oloyedammy@gmail.com>
2023-10-30 09:55:52 +0100

    other pages added

---
+++ app/components/LayoutPages.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import AnimatedWrapper from "../wrapper/AnimatedWrapper";
+import Footer from "../components/Footer";
+
+const LayoutPages = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <AnimatedWrapper>
+        <div className="max-h-screen flex flex-col justify-between">
+          <Navbar />
+          <div className="flex items-center justify-center flex-col lg:px-24 p-4">
+            {children}
+          </div>
+          <Footer />
+        </div>
+      </AnimatedWrapper>
+    </>
+  );
+};
+
+export default LayoutPages;
--- app/vip/page.tsx
+++ app/vip/page.tsx
@@ -1,22 +1,15 @@
 "use client";
 
 import React from "react";
-import Navbar from "../components/Navbar";
-import AnimatedWrapper from "../wrapper/AnimatedWrapper";
-import Footer from "../components/Footer";
+import LayoutPages from "../components/LayoutPages";
+
 
 const page = () => {
   return (
     <>
-      <AnimatedWrapper>
-        <div className="h-screen flex flex-col justify-between">
-          <Navbar />
-          <div className="flex items-center justify-center flex-col lg:px-24 p-4">
-            <h1 className="font-bold uppercase text-2xl lg:text-8xl">COMING SOON</h1>
-          </div>
-          <Footer />
-        </div>
-      </AnimatedWrapper>
+      <LayoutPages>
+        <h1 className="font-bold uppercase text-2xl lg:text-8xl">COMING SOON</h1>
+      </LayoutPages>
     </>
   );
 };
