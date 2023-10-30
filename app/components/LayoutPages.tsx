081f63e244ee54ac2c746e00587aa297f39c7869
Ibrahim Damilola Dhikirullahi <oloyedammy@gmail.com>
2023-10-30 20:15:20 +0100

    other pages added

--- app/components/LayoutPages.tsx
+++ app/components/LayoutPages.tsx
@@ -7,7 +7,7 @@ const LayoutPages = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <AnimatedWrapper>
-        <div className="max-h-screen h-screen flex flex-col justify-between">
+        <div className="overflow-hidden h-screen flex flex-col justify-between">
           <Navbar />
           <div className="flex items-center justify-center flex-col lg:px-24 p-4">
             {children}
